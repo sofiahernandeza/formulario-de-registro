e4b8c530d6273228604dec0789cc77be79a11126
UstrA <122003605+alexanderustra@users.noreply.github.com>
2023-08-10 19:08:41 -0300

    miloco funcionando

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,32 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+let formulario = document.querySelectorAll('.form-control');
+
+let contraseña1 = document.getElementById('password1');
+let contraseña2 = document.getElementById('password2');
+let terminos = document.getElementById('terminos')
+
+document.getElementById('regBtn').addEventListener('click', () => {
+    let errores = false;
+
+    for (let i = 0; i < formulario.length; i++) {
+        if (formulario[i].value === '') {
+            showAlertError()
+            errores = true;
+            break; 
+        }
+    }
+    if (contraseña1.value.length < 6 || contraseña1.value !== contraseña2.value) {
+        showAlertError()
+        errores = true;
+    }
+    if (!terminos.checked) {
+        showAlertError()
+        errores = true; 
+    }
+    if (!errores) {
+        showAlertSuccess()
+    }
+});
