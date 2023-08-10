460b31a28436685a2d6a196da2d73d48d6a1e3fa
CopiaPiratA <aleustrsa2004@gmail.com>
2023-08-10 18:55:31 -0300

    funcionando

--- js/validacion.js
+++ js/validacion.js
@@ -12,17 +12,25 @@ let contraseña1 = document.getElementById('password1');
 let contraseña2 = document.getElementById('password2');
 let terminos = document.getElementById('terminos')
 
-document.getElementById('regBtn').addEventListener('click',()=>{
+document.getElementById('regBtn').addEventListener('click', () => {
+    let errores = false;
 
-    for(let i = 0; i < formulario.length ; i++) {
-        if(formulario[i].value === '') {
-            alert("complete todos los campos")// bucle de alertas
+    for (let i = 0; i < formulario.length; i++) {
+        if (formulario[i].value === '') {
+            alert("Complete todos los campos");
+            errores = true;
+            break; 
         }
-    } 
-    if (contraseña1.length < 6 || contraseña1.value != contraseña2.value) {
-        alert('contraseñas no coinciden o no tienen suficientes caracteres')
     }
-    if(!terminos) {
-        alert('Acepte los terminos y condiciones')
+    if (contraseña1.value.length < 6 || contraseña1.value !== contraseña2.value) {
+        alert('Contraseñas no coinciden o no tienen suficientes caracteres');
+        errores = true;
     }
-})
+    if (!terminos.checked) {
+        alert('Acepte los términos y condiciones');
+        errores = true; 
+    }
+    if (!errores) {
+        alert('Formulario enviado exitosamente');
+    }
+});
